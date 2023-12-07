99a42628515b92a3ab9d33ebadc2b94559767d15
Mustafa <bhikhapurmustafa@gmail.com>
2023-12-07 23:11:26 +0530

    fixes

--- src/pages/blog/[slug].js
+++ src/pages/blog/[slug].js
@@ -36,21 +36,19 @@ const BlogPost = ({ post }) => {
     );
   }
 
-  const processedContent =  remark()
-    .use(remarkGfm)
-    .use(html)
-    .process(post.content);
-   const contentHtml = processedContent.toString();
+  const processedContent = remark().use(remarkGfm).use(html).processSync(post.content);
+  const contentHtml = processedContent.toString();
 
 
   return (
     <BaseLayout>
       <h1 className='title section-title'>{post.title}</h1>
       <p className='para padd-15'>{post.date}</p>
-      <ReactMarkdown remarkPlugins={[remarkGfm]} children={post.content} className='content padd-15' />
+      <ReactMarkdown remarkPlugins={[remarkGfm]} className='content padd-15'>
+        {contentHtml}
+      </ReactMarkdown>
     </BaseLayout>
   );
 };
-
 
 export default BlogPost;
